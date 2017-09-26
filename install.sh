7de44ebce8728d4f726b65a96d0c91ebab25d89c
n2omatt <n2omatt@amazingcow.com>
2017-09-25 22:06:50 -0300

    - Add the license headers.

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+##~---------------------------------------------------------------------------##
+##                         ____                       _   _                   ##
+##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
+##                 | '_ \  __) / _ \| '_ ` _ \ / _` | __| __|                 ##
+##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
+##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
+##                              www.n2omatt.com                               ##
+##  File      : install.sh                                                    ##
+##  Project   : bash-status-line                                              ##
+##  Date      : Apr 12, 2016                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : n2omatt - 2016, 2017                                          ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
 
-##COWTODO: Add license header....
 ##COWTODO: Make this script nicer...
 
 if [ -e $HOME/.bashrc ]; then
@@ -11,7 +27,7 @@ if [ -e $HOME/.bashrc ]; then
     GREP_RESULT=$(cat $HOME/.bashrc | grep "$FILENAME");
 
     if [ -z  "$GREP_RESULT" ]; then
-        echo "[[ -s \"$FILENAME\" ]] && source \"$FILENAME\"" >> $HOME/.bashrc;	
+        echo "[[ -s \"$FILENAME\" ]] && source \"$FILENAME\"" >> $HOME/.bashrc;
     fi;
 
     echo "Installed...";
